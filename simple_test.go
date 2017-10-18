3a381d7d0bc800c71e40b01b71bfcd1baf26c3c4
Zhou Yu <zhou@improbable.io>
2017-10-18 18:33:57 +0100

    Remove the advanced examples for now

--- simple_test.go
+++ simple_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/jutkko/mockComp/mockCompfakes"
 	"github.com/jutkko/mockComp/mockgen"
 	"github.com/jutkko/mockComp/mocks"
-	"github.com/stretchr/testify/mock"
 )
 
 func TestSimpleCounterfeiterPerson(t *testing.T) {
@@ -55,50 +54,4 @@ func TestSimpleGogenPerson(t *testing.T) {
 
 	// The actual call
 	fakePerson.DoThings("Hi it's 24", 24)
-}
-
-func TestAdvancedCounterfeiterPerson(t *testing.T) {
-	var fakePerson = new(mockCompfakes.FakePerson)
-
-	// The function is type checked
-	fakePerson.DoThingsStub = func(inputString string, inputInt int) (int, error) {
-		if inputString == "Hi it's 24" {
-			return inputInt + 1, nil
-		}
-
-		return 0, errors.New("An error")
-	}
-
-	res, err := fakePerson.DoThings("Hi it's 24", 24)
-	// Do you assertion on res and err
-	if res != 25 || err != nil {
-		t.Fatal("Wrong result")
-	}
-
-	// Get the args from the call, this is an array
-	firstArg, secondArg := fakePerson.DoThingsArgsForCall(0)
-	if firstArg != "Hi it's 24" || secondArg != 24 {
-		t.Fatal("Wrong result")
-	}
-}
-
-func TestAdvancedMockeryPerson(t *testing.T) {
-	var fakePerson = &mocks.Person{}
-
-	// The function is not type checked
-	doThingsStub := func(inputString string, inputInt int) (int, error) {
-		if inputString == "Hi it's 24" {
-			return inputInt + 1, nil
-		}
-
-		return 0, errors.New("An error")
-	}
-
-	fakePerson.On("DoThings", mock.Anything, mock.Anything).Return(doThingsStub)
-
-	res, err := fakePerson.DoThings("Hi it's 24", 24)
-	// Do you assertion on res and err
-	if res != 42 || err == nil {
-		t.Fatal("Wrong result")
-	}
 }
